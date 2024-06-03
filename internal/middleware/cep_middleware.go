bcff93f3dacd539f4fdc09b378dc23f1049b1fbe
Bruno Oliveira <58921482+brunoliveiradev@users.noreply.github.com>
2024-06-03 01:07:28 -0300

    [FEATURE]: Updated the Cep Middleware

--- internal/middleware/cep_middleware.go
+++ internal/middleware/cep_middleware.go
@@ -9,7 +9,12 @@ import (
 func CheckCepMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cep := chi.URLParam(r, "cep")
-		if cep == "" || !utils.IsValidZipcode(cep) {
+		if cep == "" || len(cep) == 0 {
+			http.Error(w, "CEP is required", http.StatusBadRequest)
+			return
+		}
+
+		if !utils.IsValidZipcode(cep) {
 			http.Error(w, "invalid zipcode", http.StatusUnprocessableEntity)
 			return
 		}
